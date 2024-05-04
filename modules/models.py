0082d41bbbd5cd5aeb67c653022e5472805f1297
Harry2687 <harry.zhong2687@gmail.com>
2024-05-05 00:26:51 +0800

    Added resnet 64

--- modules/models.py
+++ modules/models.py
@@ -409,3 +409,50 @@ class resnetModel_128(nn.Module):
         x = self.classifier(x)
         x = F.softmax(x, dim=1)
         return x
+    
+class resnetModel_64(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv_1 = conv_block(1, 64)
+        self.res_1 = nn.Sequential(
+            conv_block(64, 64), 
+            conv_block(64, 64)
+        )
+        self.conv_2 = conv_block(64, 256, pool=True)
+        self.res_2 = nn.Sequential(
+            conv_block(256, 256),
+            conv_block(256, 256)
+        )
+        self.conv_3 = conv_block(256, 512, pool=True)
+        self.res_3 = nn.Sequential(
+            conv_block(512, 512),
+            conv_block(512, 512)
+        )
+        self.conv_4 = conv_block(512, 1024, pool=True)
+        self.res_4 = nn.Sequential(
+            conv_block(1024, 1024),
+            conv_block(1024, 1024)
+        )
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(1024, 512),
+            nn.Dropout(0.5),
+            nn.ReLU(),
+            nn.Linear(512, 256),
+            nn.Dropout(0.5),
+            nn.ReLU(),
+            nn.Linear(256, 2)
+        )
+    
+    def forward(self, x):
+        x = self.conv_1(x)
+        x = self.res_1(x) + x
+        x = self.conv_2(x)
+        x = self.res_2(x) + x
+        x = self.conv_3(x)
+        x = self.res_3(x) + x
+        x = self.conv_4(x)
+        x = self.res_4(x) + x
+        x = self.classifier(x)
+        x = F.softmax(x, dim=1)
+        return x
