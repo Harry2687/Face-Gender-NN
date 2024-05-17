5118b3083e3d0cb45e56a1b26a2884ba000f0d3f
Harry2687 <harry.zhong2687@gmail.com>
2024-05-17 21:41:24 +0800

    fixed gdown output path

--- trained_models/model_download.py
+++ trained_models/model_download.py
@@ -1,5 +1,6 @@
 import gdown
 
 url = 'https://drive.google.com/file/d/1_mYn2LrhG080Xvt26tWBtJ8U_0F2E1-s/view?usp=sharing'
+output = 'trained_models/resnetModel_128_epoch_2.pt'
 
-gdown.download(url, fuzzy=True)
+gdown.download(url, output, fuzzy=True)
